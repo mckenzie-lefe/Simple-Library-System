0464e045731d8bacad8b629624cd20b8cf83854c
McKenzie Lefebvre <109600778+mckenzie-lefe@users.noreply.github.com>
2023-10-05 17:04:58 -0600

    V3 - Collapse Hierarchy
    Delete BookForSale class and its tests.

--- AllTests.java
+++ AllTests.java
@@ -3,7 +3,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({TestBook.class, TestBookForBorrow.class, TestBookForSale.class, 
-    TestLibrarySystem.class, TestLibraryMember.class, TestRating.class})
+@SuiteClasses({TestBook.class, TestBookForBorrow.class, TestLibrarySystem.class, 
+    TestLibraryMember.class, TestRating.class})
 
 public class AllTests {}
